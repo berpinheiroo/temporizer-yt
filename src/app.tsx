360f57599a7b2cffc1f848a8fc7d5caee20afaa3
Bernardo Pinheiro <berpinheiroo@gmail.com>
2025-04-26 12:29:06 -0300

    refactor: remove template codes

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,6 @@
-import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
-import { queryClient } from './@config/lib/react-query'
 import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
 import { router } from './router'
@@ -10,16 +8,14 @@ import { router } from './router'
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Template Nitro HUB Frontend" />
+      <Helmet titleTemplate="%s | Temporizer" />
       <ThemeProvider
-        storageKey="template-nitro-hub-frontend"
+        storageKey="temporizer"
         defaultTheme="dark"
       >
-        <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
 
           <Toaster richColors position="bottom-left" />
-        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
